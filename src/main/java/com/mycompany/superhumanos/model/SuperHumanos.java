9fce6622bb9c9fa5cf708c97f634c8f12c63e2fc
leonaidasup <leonardo.amaris@udea.edu.co>
2023-09-13 09:00:45 -0500

    feat: creacion del proyecto y sus carpetas

---
+++ src/main/java/com/mycompany/superhumanos/model/SuperHumanos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.superhumanos.model;
+
+/**
+ *
+ * @author Leonardo.amaris
+ */
+public class SuperHumanos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
