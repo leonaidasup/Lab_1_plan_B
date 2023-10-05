966c0250a4c5a15da67fc5c39934714eec9f40e7
Johanna <johamon19@gmail.com>
2023-10-05 14:27:22 -0500

    FaltaGuerra

--- src/main/java/Control/DatosHumanosController.java
+++ src/main/java/Control/DatosHumanosController.java
@@ -14,6 +14,9 @@ import javax.swing.JOptionPane;
 public class DatosHumanosController {
     ArrayList<SuperHumano> Admitidos;
     ArrayList<SuperHumano> Denegados;
+    ArrayList<SuperHumano> Escuadron1;
+    ArrayList<SuperHumano> Escuadron2;
+    ArrayList<SuperHumano> Reserva;
     
     public static VentanaDatosHumanos ventana = new VentanaDatosHumanos();
     public static void mostrar(){ ventana.setVisible(true);}
@@ -22,15 +25,16 @@ public class DatosHumanosController {
     public DatosHumanosController(){
         Admitidos=new ArrayList();
         Denegados=new ArrayList();
+        Escuadron1=new ArrayList();
+        Escuadron2=new ArrayList();
+        Reserva=new ArrayList();
     }
     
     public String DevolverAdmitidos()
     {
         String reporte="";
-        for(int contador=0;contador<Admitidos.size();contador++)
-        {
-            reporte+=Admitidos.get(contador).toString()+"\n";
-        }
+        for(int contador=0;contador<Admitidos.size();contador++){
+            reporte+=Admitidos.get(contador).toString()+"\n";}
         return reporte;
     }
 
@@ -79,35 +83,47 @@ public class DatosHumanosController {
 
             SuperHumano persona = new SuperHumano(superNombre, saludMental, superFuerza, visionLaser, nombre,edad,estadoCivil,hijos,DNI);
             
-            if(edad<50 && estadoCivil.equals("SOLTERO")){
-                if(saludMental<2){
-                    if(superFuerza > 1000 || visionLaser.equals("SI")){
+            if(saludMental<2){ // Si la salud mental es buena, entonces...
+                if(superFuerza > 1000 || visionLaser.equals("SI")){ // Si tiene poderes, entonces...
+                   if((edad<50 && estadoCivil.equals("SOLTERO") && hijos < 1) && Escuadron1.size() < 2){ // Si es menor de 50, esta soltero, no tiene hijos y el tamaño del escuadron 1 es menor a 15, entonces...
                         Admitidos.add(persona);
-                    } else {
-                        Denegados.add(persona);
-                    }                    
-                } else {
-                    Denegados.add(persona);
-                }                             
-            } else {
+                        Escuadron1.add(persona);
+                        System.out.println("Ingresa al escuadron 1");
+                    }else if( hijos > 1 || (Escuadron1.size() >= 2 && Escuadron2.size() < 2)){ // puede tener hijos y el escuadron 2 es menor a 15 ó no tiene hijos pero el escuadron 1 esta lleno, entonces...
+                        Admitidos.add(persona);
+                        Escuadron2.add(persona);
+                        System.out.println("Ingresa al escuadron 2");
+                    }else{ // si los escuadrones estan llenos, entonces...
+                        Reserva.add(persona);
+                        Admitidos.add(persona);
+                        System.out.println("Ingresas a reserva porque los escuadrones principales estan llenos");     
+                    } // finaliza else de escuadrones
+                } else { // Si no tiene poderes, entonces...
                 Denegados.add(persona);
-            }
-            
+                System.out.println("Denegado porque no tiene poderes");
+                } // finaliza else de poderes
+            } else { // Si la salud Mental es mala, entonces...
+                Denegados.add(persona);
+                System.out.println("Denegado por SaludMental");
+            }// finaliza else de salud mental
+
             JOptionPane.showMessageDialog(null, "¡Datos guardados correctamente!");
         }
         catch(NumberFormatException ex){
-            JOptionPane.showMessageDialog(null, "ERROR. Debes introducir un número en edad e hijos");
+            JOptionPane.showMessageDialog(null, "ERROR. Debes introducir un número en edad, hijos y Prueba de fuerza");
         }
         catch(Exception ex){
             JOptionPane.showMessageDialog(null, "ERROR. fallo no identificado"+ ex.getMessage());
         }
     }
+    
+    
     public void BotonIrAlfis(){
-        //if (personas.size() > 20){
+        //if (admitidos.size() > 20){
             ocultar();
             DatosAlfisController.mostrar();
         /*} else {
-            JOptionPane.showMessageDialog(null, "Tiene que ingresar los datos de al menos 20 personas");
+            JOptionPane.showMessageDialog(null, "Tiene que haber al menos 20 SuperHumanos Admitidos");
         }*/
     
     }
